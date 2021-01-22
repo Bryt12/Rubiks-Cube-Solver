01fe5ec18d2122a5d9b26b674280c77d1cd9817b
Bryson Tang <bmtang@wpi.edu>
2021-01-22 04:03:16 -0500

    + Solver code

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import arcade
 import math
 from cube import Cube
+from solver import Solver
+import matplotlib.pyplot as plt
 
 sticker_width = 50
 
-WIDTH = sticker_width*12
-HEIGHT = sticker_width*9
+WIDTH = sticker_width * 12
+HEIGHT = sticker_width * 9
+
+NUMBER_FOR_SCRAMBLE = 30
+
+NUMBER_OF_CUBES_TO_TRAIN = 50
+NUMBER_OF_TURNS_PER_CUBE = 30
+NUMBER_OF_TRAINS = 20
+
 
 class CubeDisplay(arcade.Window):
     scrambling = False
     scrambles = 0
 
+    collecting_data = False
+    collections = 0
+    trains = 0
+    cubes_scrambled = 0
+
+    solving = False
+
+    max_scores = []
+    rand_counts = []
+
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
         super().__init__(WIDTH, HEIGHT, "Cube")
-
         arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
@@ -24,9 +42,59 @@ class CubeDisplay(arcade.Window):
         if self.scrambling:
             cube.scramble(1)
             self.scrambles += 1
-        if self.scrambles > 30:
+        if self.scrambles >= NUMBER_FOR_SCRAMBLE:
             self.scrambling = False
             self.scrambles = 0
+
+        if self.collecting_data:
+            # After the number of turns per cube is up re-scramble
+            if self.collections % NUMBER_OF_TURNS_PER_CUBE == 0:
+                cube.solve()
+                cube.scramble(int(NUMBER_FOR_SCRAMBLE * (self.cubes_scrambled/NUMBER_OF_CUBES_TO_TRAIN)))
+                self.cubes_scrambled += 1
+            solver.generate_move(cube)
+
+            self.collections += 1
+
+            # If at end of training cycle
+            if self.collections % (NUMBER_OF_CUBES_TO_TRAIN * NUMBER_OF_TURNS_PER_CUBE) == 0:
+                self.trains += 1
+                self.cubes_scrambled = 1
+                rand_count = solver.train_model()
+
+                # Test model
+                cube.scramble(NUMBER_FOR_SCRAMBLE)
+                max_score = 0
+                for _ in range(20):
+                    solver.generate_move(cube)
+                    if cube.score() > max_score:
+                        max_score = cube.score()
+
+                self.max_scores.append(max_score / 54)
+                self.rand_counts.append(rand_count)
+                print("Generation Stats")
+                print(str(int(self.collections / (NUMBER_OF_CUBES_TO_TRAIN * NUMBER_OF_TURNS_PER_CUBE))) + "/" + str(NUMBER_OF_TRAINS))
+                print("Max completion:" + str(max_score / 54) + "%")
+                print("Percent Random:" + str(rand_count / NUMBER_OF_TURNS_PER_CUBE) + "%")
+
+        # If done training
+        if self.trains >= NUMBER_OF_TRAINS:
+            self.trains = 0
+            print(self.collections)
+            fig, axs = plt.subplots(2)
+            axs[0].plot(self.max_scores)
+            axs[0].set_ylabel('Percent Complete')
+            axs[1].plot(self.rand_counts)
+            axs[1].set_ylabel('Random Count')
+            plt.show()
+
+            self.collecting_data = False
+            self.collections = 0
+
+            cube.solve()
+
+        if self.solving:
+            solver.generate_move(cube)
 
         side_offset_left = [sticker_width * 3,
                             sticker_width * 3,
@@ -51,10 +119,10 @@ class CubeDisplay(arcade.Window):
                 sticker_y = math.floor(sticker / 3)
 
                 x = side_offset_left[side] + (sticker_width * sticker_x)
-                y = side_offset_top[side] + (sticker_width * abs(2-sticker_y))
+                y = side_offset_top[side] + (sticker_width * abs(2 - sticker_y))
 
-                x_center = x + (sticker_width/2)
-                y_center = y + (sticker_width/2)
+                x_center = x + (sticker_width / 2)
+                y_center = y + (sticker_width / 2)
                 col = self.get_color(cube.cube[side].T[sticker_x][sticker_y])
 
                 arcade.draw_rectangle_filled(x_center, y_center, sticker_width, sticker_width, col)
@@ -79,6 +147,10 @@ class CubeDisplay(arcade.Window):
                 self.scrambling = True
             else:
                 cube.solve()
+        if symbol == arcade.key.T:
+            self.collecting_data = True
+        if symbol == arcade.key.A:
+            self.solving = not self.solving
 
     def get_color(self, char):
         if char == 'w':
@@ -88,17 +160,19 @@ class CubeDisplay(arcade.Window):
         if char == 'o':
             return arcade.color.PASTEL_ORANGE
         if char == 'b':
-            return arcade.color.PASTEL_BLUE
+            return arcade.color.BLUEBERRY
         if char == 'r':
             return arcade.color.PASTEL_RED
         if char == 'y':
             return arcade.color.PASTEL_YELLOW
 
+
 def main():
     window = CubeDisplay()
     arcade.run()
 
+
 if __name__ == "__main__":
     cube = Cube()
-
+    solver = Solver()
     main()
--- solver.py
+++ solver.py
@@ -1,6 +1,97 @@
-import cube
+from cube import Cube
 from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+import pandas as pd
+import math
+import random
+import numpy as np
 
+class Solver:
+    def __init__(self):
+        self.X_train = []
+        self.y_train = []
+        self.last_two = [None, None]
+        self.model = self.make_model()
+        self.rand_count = 0
 
-moves_to_try = 100
+    def make_model(self):
+        model = Sequential()
+        model.add(Dense(256, input_dim=324, activation='relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dense(12, activation='softmax'))
 
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+
+        return model
+
+    def preprocess_cube(self, c):
+        c = c.reshape((1,54))
+        df = pd.DataFrame(c)
+        df = df.apply(lambda x: x.astype('category').cat.set_categories(['w', 'g', 'o', 'b', 'r', 'y']))
+        df = pd.get_dummies(df)
+        return df
+
+    def number_to_move(self, num):
+        if num == 0 or num == 1:
+            return 'r', num%2 == 0
+        if num == 2 or num == 3:
+            return 'l', num%2 == 0
+        if num == 4 or num == 5:
+            return 'u', num%2 == 0
+        if num == 6 or num == 7:
+            return 'd', num%2 == 0
+        if num == 8 or num == 9:
+            return 'f', num%2 == 0
+        if num == 10 or num == 11:
+            return 'b', num%2 == 0
+
+    def generate_move(self, cube):
+        init_state = cube.cube.copy()
+        init_score = cube.score()
+
+        df = self.preprocess_cube(cube.cube)
+        num = np.argmax(self.model.predict(df), axis=-1)[0]
+
+        if self.last_two[1] is not None:
+            last_move, last_clockwise = self.number_to_move(self.last_two[1])
+        else:
+            last_move, last_clockwise = -99, -99
+        this_move, this_clockwise = self.number_to_move(num)
+
+        is_reverse_of_last_move = (last_move == this_move and this_clockwise != last_clockwise)
+        three_of_same_in_row = (num == self.last_two[0] and num == self.last_two[1])
+        if three_of_same_in_row or is_reverse_of_last_move:
+            self.rand_count += 1
+            num = random.randint(0, 11)
+
+        self.last_two[0] = self.last_two[1]
+        self.last_two[1] = num
+
+        move, clockwise = self.number_to_move(num)
+
+        cube.make_move(move, clockwise)
+
+        current_score = cube.score()
+
+        if current_score > init_score:
+            self.X_train.append(self.preprocess_cube(init_state).iloc[0])
+            self.y_train.append(to_categorical(num, num_classes=12))
+
+        return move, clockwise
+
+    def train_model(self):
+        rand_c = self.rand_count
+
+        self.rand_count = 0
+
+        if len(self.y_train) > 0:
+            self.model = self.make_model()
+
+            self.model.fit(np.asarray(self.X_train), np.asarray(self.y_train), epochs=30, verbose=0)
+
+        return rand_c
